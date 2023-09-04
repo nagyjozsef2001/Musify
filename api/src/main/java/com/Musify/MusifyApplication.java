9bb03b32c4e1ee901b4a5cd2650c6156f43c6e57
Jozsef Nagy <nagyjozsef2001@gmail.com>
2023-09-04 16:46:58 +0300

    functional maven project

---
+++ api/src/main/java/com/Musify/MusifyApplication.java
@@ -0,0 +1,13 @@
+package com.Musify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusifyApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/Musify/MusifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Musify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
